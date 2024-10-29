2dcff81746bdb867350337347ae4491c2bcc3807
José Santos <sjose7780@gmail.com>
2024-10-29 16:39:55 +0000

    Setup done

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
-docker run --name my_solr -d -p 8983:8983 solr:9 
-docker exec pri_solr solr create_core -c ptnews
+# docker run --name pri_solr -d -p 8983:8983 -v "/mnt/c/Users/Diogo Monteiro/Desktop/data:/data" solr
+docker run --name pri_solr -d -p 8983:8983 -v "C:\Users\User\OneDrive\Documentos\PRI\pri/data:/data" solr
+docker exec pri_solr solr create_core -c covid
+docker exec -it pri_solr bash
+curl -X POST -H "Content-Type: application/json" --data-binary "@/data/cord-19.json" "http://localhost:8983/solr/covid/update?commit=true" &
