3e5f3ff8a1341b495a6ae613ba054f4eee748449
Diogo Monteiro <up202108800@up.pt>
2024-11-12 16:43:51 +0000

    Added schema to data

--- script.sh
+++ script.sh
@@ -1,5 +1,14 @@
-# docker run --name pri_solr -d -p 8983:8983 -v "/mnt/c/Users/Diogo Monteiro/Desktop/data:/data" solr
-docker run --name pri_solr -d -p 8983:8983 -v "C:\Users\User\OneDrive\Documentos\PRI\pri/data:/data" solr
+python3 add_embeddings_to_json.py < data/cord-19.json > data/updated_cord-19.json
+
+docker run --name pri_solr -d -p 8983:8983 -v "/mnt/c/Users/Diogo Monteiro/Desktop/pri/data:/data" solr
+# docker run --name pri_solr -d -p 8983:8983 -v "C:\Users\User\OneDrive\Documentos\PRI\pri/data:/data" solr
+
+#docker run --name pri_solr -d -p 8983:8983 -v "C:\Users\sjose\OneDrive\Documentos\pri2024/data:/data" solr
+sleep(3)
 docker exec pri_solr solr create_core -c covid
+
 docker exec -it pri_solr bash
+curl -X POST -H 'Content-type: application/json' --data-binary "@/data/schema_default.json" "http://localhost:8983/solr/covid/schema" &
+
+sleep(3)
 curl -X POST -H "Content-Type: application/json" --data-binary "@/data/cord-19.json" "http://localhost:8983/solr/covid/update?commit=true" &
