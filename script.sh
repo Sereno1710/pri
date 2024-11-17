b8a9c51cde466e8582e113ed87afd69f84e67b75
José Santos <sjose7780@gmail.com>
2024-11-17 14:20:11 +0000

    Finished schemas?

--- script.sh
+++ script.sh
@@ -5,14 +5,15 @@ docker run --name pri_solr -d -p 8983:8983 -v "/mnt/c/Users/Diogo Monteiro/Deskt
 
 #docker run --name pri_solr -d -p 8983:8983 -v "C:\Users\sjose\OneDrive\Documentos\pri2024/data:/data" solr
 sleep(3)
-
+docker exec pri_solr bin/solr delete -c covid
 
 docker cp synonyms.txt pri_solr:/var/solr/data/covid/conf
+docker cp stop_words.txt pri_solr:/var/solr/data/covid/conf
 
 docker exec pri_solr solr create_core -c covid
 
 docker exec -it pri_solr bash
-curl -X POST -H 'Content-type: application/json' --data-binary "@/data/schema_default.json" "http://localhost:8983/solr/covid/schema" &
+curl -X POST -H 'Content-type: application/json' --data-binary "@/data/schema_stemmer.json" "http://localhost:8983/solr/covid/schema" &
 
 sleep(3)
-curl -X POST -H "Content-Type: application/json" --data-binary "@/data/cord-19.json" "http://localhost:8983/solr/covid/update?commit=true" &
+curl -X POST -H "Content-Type: application/json" --data-binary "@/data/updated_cord-19.json" "http://localhost:8983/solr/covid/update?commit=true" &
