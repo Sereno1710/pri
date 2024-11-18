8e86485ba797093193590209a1c7b7023c598937
José Santos <sjose7780@gmail.com>
2024-11-18 18:25:44 +0000

    A

--- lemma_schema.sh
+++ lemma_schema.sh
@@ -11,7 +11,7 @@ docker exec pri_solr solr delete_core -c covid
 docker exec pri_solr solr create_core -c covid
 
 docker cp synonyms.txt pri_solr:/var/solr/data/covid/conf
-docker cp stop_words.txt pri_solr:/var/solr/data/covid/conf
+docker cp stopwords.txt pri_solr:/var/solr/data/covid/conf
 
 docker exec -it pri_solr bash
 curl -X POST -H 'Content-type: application/json' --data-binary "@/data/schema_lemma.json" "http://localhost:8983/solr/covid/schema" &
